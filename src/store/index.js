762cd9add87a3a75379861651a07bb520b462060
Dickson Amenyo <36271402+dkerobean@users.noreply.github.com>
2024-10-21 10:16:38 +0000

    new tempolate

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./rootReducer";
+import rootReducer from './rootReducer';
+import { configureStore } from '@reduxjs/toolkit';
+
+const middlewares = [];
 
 const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware({
-      serializableCheck: false,
-    });
-  },
-});
+	reducer: rootReducer(),
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			immutableCheck: false,
+			serializableCheck: false,
+		}).concat(middlewares),
+	devTools: process.env.NODE_ENV === 'development',
+})
 
-export default store;
+store.asyncReducers = {};
+
+export const injectReducer = (key, reducer) => {
+	if (store.asyncReducers[key]) {
+		return false;
+	}
+	store.asyncReducers[key] = reducer;
+	store.replaceReducer(rootReducer(store.asyncReducers));
+	return store
+}
+
+export default store
